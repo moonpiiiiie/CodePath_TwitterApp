14acc4f0c9b8ae267d33e9bccf809d92a3716f34
Cheng Xue <cheng.xue910@gmail.com>
2022-09-29 22:04:35 -0700

    update gif

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import RSKPlaceholderTextView
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var tweetTextView: UITextView!
+    
+    
+    @IBOutlet var wordCount: UILabel!
+    
     
     @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true)
@@ -24,15 +29,28 @@ class TweetViewController: UIViewController {
                 self.dismiss(animated: true)
             })
         }
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        tweetTextView.delegate = self
         tweetTextView.becomeFirstResponder()
+//        wordCount.text = "140"
     }
     
+//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//       // TODO: Check the proposed new text character count
+//       // Allow or disallow the new text
+//        let characterLimit = 140
+//
+//        // Construct what the new text would be if we allowed the user's latest edit
+//        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+//        self.wordCount.text = String(newText.count)
+//        // TODO: Update Character Count Label
+//        // The new text should be allowed? True/False
+//
+//        return newText.count < characterLimit
+//    }
 
 
 
